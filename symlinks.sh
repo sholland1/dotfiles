edd8c268d9063a5882c7076ee405586f178405bf
Seth Holland <holland_s91@yahoo.com>
2019-06-15 01:33:31 -0500

    Rename vifm colors file

--- symlinks.sh
+++ symlinks.sh
@@ -12,11 +12,11 @@ rm -rdf .local/share/sidewinderd
 ln -s ~/dotfiles/.local/share/sidewinderd .local/share/sidewinderd
 
 cd ~/.config
-rm nvim/init.vim onedrive/config sxhkd/sxhkdrc vifm/vifmrc vifm/colors/one.vifm termite/config
+rm nvim/init.vim onedrive/config sxhkd/sxhkdrc vifm/vifmrc vifm/colors/terminal.vifm termite/config
 ln -s ~/dotfiles/.config/nvim/init.vim nvim/init.vim
 ln -s ~/dotfiles/.config/onedrive/config onedrive/config
 ln -s ~/dotfiles/.config/sxhkd/sxhkdrc sxhkd/sxhkdrc
 ln -s ~/dotfiles/.config/vifm/vifmrc vifm/vifmrc
-ln -s ~/dotfiles/.config/vifm/colors/one.vifm vifm/colors/one.vifm
+ln -s ~/dotfiles/.config/vifm/colors/terminal.vifm vifm/colors/terminal.vifm
 ln -s ~/dotfiles/.config/termite/config termite/config
 
