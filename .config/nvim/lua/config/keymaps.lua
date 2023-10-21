fd9196fb8a933b350fb3e250367b4ed086a9b8ab
Seth Holland <holland_s91@yahoo.com>
2023-10-21 14:49:59 -0500

    Switch to LazyVim

---
+++ .config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,47 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+-- Define a function to toggle the checkbox on the current line
+local function toggleCheckbox()
+  local current_line = vim.fn.line(".")
+  local line_content = vim.fn.getline(current_line)
+
+  -- Toggle the checkbox indicator by adding or removing "x"
+  local updated_line
+  if line_content:match("%[x%]") then
+    updated_line = line_content:gsub("%[x%]", "[ ]")
+  else
+    updated_line = line_content:gsub("%[ %]", "[x]")
+  end
+
+  -- Update the line with the modified content
+  vim.fn.setline(current_line, updated_line)
+end
+
+local km = vim.keymap
+
+km.set("n", "<leader>d", "<cmd>bd<cr>", { desc = "Close Buffer" })
+km.set("n", "<leader>t", toggleCheckbox, { desc = "Toggle Checkbox", noremap = true, silent = true })
+km.set("n", "<leader>q", "<cmd>q<cr>", { desc = "Close Window" })
+km.set("n", "<leader>v", "<cmd>Vifm fnameescape(expand('%:p:h')) fnameescape(getcwd())<cr>", { desc = "Open Vifm" })
+km.set("n", "<leader>w", "<cmd>w<cr>", { desc = "Write", remap = true })
+
+km.set("n", "<leader>ww", "<nop>", { remap = true })
+km.set("n", "<leader>wd", "<nop>", { remap = true })
+km.set("n", "<leader>w-", "<nop>", { remap = true })
+km.set("n", "<leader>w|", "<nop>", { remap = true })
+--km.set("n", "<leader>c", "<nop>", { remap = true })
+
+km.set("n", "<cr>", "o<esc>")
+km.set("n", "s", "s")
+km.set("n", "<S-tab>", "<C-w>W")
+km.set("n", "<tab>", "<C-w>w")
+km.set("n", "<C-v>", "<C-v>")
+
+km.set("n", "H", "H", { remap = true })
+km.set("n", "L", "L", { remap = true })
+km.set({ "i", "n", "v" }, "<C-h>", "<cmd>bprev<cr>", { remap = true })
+km.set({ "i", "n", "v" }, "<C-l>", "<cmd>bnext<cr>", { remap = true })
+km.set({ "i", "n", "v" }, "<C-PageUp>", "<cmd>bprev<cr>", { remap = true })
+km.set({ "i", "n", "v" }, "<C-PageDown>", "<cmd>bnext<cr>", { remap = true })
