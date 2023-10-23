e97e56eae8dc50e5c9bb3e945715d0e40dc20b3d
Seth Holland <holland_s91@yahoo.com>
2023-10-23 17:53:11 -0500

    Add old autocmds in lua

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,18 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+local function createAutocmd(pattern, command)
+  vim.api.nvim_create_autocmd("BufWritePost", {
+    group = "userGroup",
+    pattern = pattern,
+    command = command,
+  })
+end
+
+vim.api.nvim_create_augroup("userGroup", { clear = true })
+
+createAutocmd("*.lua", "luafile %")
+createAutocmd(".zshrc", "!source %")
+createAutocmd(".compton.conf", "!pkill picom;picom &")
+createAutocmd("sxhkdrc", "!pkill sxhkd;sxhkd &")
